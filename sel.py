0e5339c1530e05f84b82408ffef6273cc353ebf9
mun9769 <mun9769@users.noreply.github.com>
2020-12-21 19:43:29 +0900

    open downloader app 다운할때 중간에 뜨는 광고 Disabled하게 바꾸자

--- sel.py
+++ sel.py
@@ -1,17 +1,31 @@
 from selenium import webdriver
 import pyautogui
 import pyperclip
+import subprocess
+import threading
+# 어플 호출
+class Application(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    
+    def run(self):
+        subprocess.call("C://Program Files//4KDownload//4kvideodownloader//4kvideodownloader.exe")
+app = Application()
+app.start()
+
 # pyautogui.PAUSE = 3
 
-url = "https://www.youtube.com/playlist?list=PLdEdazAwz5Q_n47tqf0QY94ASCmWqeGX1"
+url = input("추가한 재생목록의 url을 가져오세요 :")
 browser = webdriver.Chrome() 
 browser.get(url)
 
 # with open("ytb_list.html","w",encoding="utf-8") as f:
 #     f.write(browser.page_source)
 
+
 elements = browser.find_elements_by_class_name('yt-simple-endpoint')
 lst =[]
+
 
 for idx, element in enumerate(elements):
     if "https://www.youtube.com/watch?" in str(element.get_attribute("href")):
@@ -21,13 +35,14 @@ print(lst)
 print(len(lst))
 
 
+
 w = pyautogui.getWindowsWithTitle("4K Video Downloader – 비활성화")[0]
 
-# print(w)
+print(w)
 if w.isActive == False:
     w.activate()
 
 for url in lst:
     pyperclip.copy(str(url))
-    pyautogui.click(w.left+40,w.top+100,duration= 1)
+    pyautogui.click(w.left+40,w.top+100,duration= 3)
     pyautogui.click(w.left+480,w.top+480,duration= 1)
